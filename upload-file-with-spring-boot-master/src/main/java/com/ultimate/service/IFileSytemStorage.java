01f48b1edd663fd06657a3fb97c2dd3b8d15d0d4
JunaidKhan3 <86572174+JunaidKhan3@users.noreply.github.com>
2022-12-19 13:02:33 +0530

    Add files via upload

---
+++ upload-file-with-spring-boot-master/src/main/java/com/ultimate/service/IFileSytemStorage.java
@@ -0,0 +1,14 @@
+package com.ultimate.service;
+
+import org.springframework.core.io.Resource;
+import org.springframework.web.multipart.MultipartFile;
+
+public interface IFileSytemStorage {
+
+    void init();
+
+    String saveFile(MultipartFile file);
+
+    Resource loadFile(String fileName);
+
+}
